ca9ef48894a4653922a590468032ff973bc34519
khushal0009 <98963826+khushal0009@users.noreply.github.com>
2022-12-31 13:15:35 +0530

    Added solution - LeetHub

---
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Sol obj = new Sol();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Sol
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+        // code here.
+        Node dummy=new Node(1), current=dummy;
+        Node temp1=head1,temp2=head2;
+        while(temp1!=null && temp2!=null){
+            if(temp1.data==temp2.data){
+                Node new_node= new Node(temp1.data);
+                current.next=new_node;
+                current=current.next;
+                temp1=temp1.next;
+                temp2=temp2.next;
+            }
+            else if(temp1.data<temp2.data){
+                temp1=temp1.next;
+            }else if(temp1.data > temp2.data){
+                temp2=temp2.next;
+            }
+        }
+        return dummy.next;
+    }
+}
