5bfdc58dffbb1d7f5e707ce2cc72b710f08a796b
khushal0009 <98963826+khushal0009@users.noreply.github.com>
2023-01-12 15:29:23 +0530

    Time: 1 ms (100.00%), Space: 45.2 MB (74.72%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null || head.next==null){
+            return ;
+        }
+        //head of first half
+        ListNode l1=head;
+        
+        //head of second half
+        ListNode slow=head;
+        
+        //tail of second half
+        ListNode fast=head;
+        
+        //tail of first half
+        ListNode prev=null;
+        
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null; 
+        
+        ListNode l2=reverse(slow);
+        
+        merge(l1,l2);
+        
+    }
+    
+    public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode cur=head;
+        
+        while(cur!=null){
+            ListNode nex=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=nex;
+        }
+        return prev;
+    }
+    
+    public void merge(ListNode l1,ListNode l2){
+        while(l1!=null){
+            ListNode l1_next=l1.next;
+            ListNode l2_next=l2.next;
+            
+            l1.next=l2;
+            if(l1_next==null){
+                break;
+            }
+            l2.next=l1_next;
+            l1=l1_next;
+            l2=l2_next;
+        }
+    }
+    
+}
