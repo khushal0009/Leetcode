08bb2252c22d9a04c3b4a5c6a87d1f151e30e716
khushal0009 <98963826+khushal0009@users.noreply.github.com>
2023-05-25 16:47:02 +0530

    Added solution - LeetHub

--- Count the Reversals - GFG/count-the-reversals.java
+++ Count the Reversals - GFG/count-the-reversals.java
@@ -34,28 +34,33 @@ class Sol
         if(s.length()%2!=0){
             return -1;
         }
-        Stack<Character> stack=new Stack<Character>();
+        // Stack<Character> stack=new Stack<Character>();
+         int ans=0,open=0,close=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='{'){
-                stack.push(ch);
+                // stack.push(ch);
+                open+=1;
             }else{
-                if(stack.isEmpty()|| stack.peek()=='}'){
-                    stack.push(ch);
+                // if(stack.isEmpty()|| stack.peek()=='}'){
+                    // stack.push(ch);
+                    if(open<=0){
+            close++;
                 }else{
-                    stack.pop();
+                    // stack.pop();
+                    open--;
                 }
             }
         }
-        int ans=0,open=0,close=0;
-        while(!stack.isEmpty()){
-            char top=stack.pop();
-            if(top=='{'){
-                open++;
-            }else{
-                close++;
-            }
-        }
+        // int ans=0,open=0,close=0;
+        // while(!stack.isEmpty()){
+        //     char top=stack.pop();
+        //     if(top=='{'){
+        //         open++;
+        //     }else{
+        //         close++;
+        //     }
+        // }
         ans=(int)(Math.ceil(open/2.0)+Math.ceil(close/2.0));
         return ans;
     }
