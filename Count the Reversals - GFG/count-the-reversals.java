6f891810b090628cdd18c7940f023162b36817b8
khushal0009 <98963826+khushal0009@users.noreply.github.com>
2023-05-25 16:38:23 +0530

    Added solution - LeetHub

---
+++ Count the Reversals - GFG/count-the-reversals.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+    		System.out.println (new Sol().countRev (s));
+        }
+        
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    int countRev (String s)
+    {
+        // your code here       
+        if(s.length()%2!=0){
+            return -1;
+        }
+        Stack<Character> stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='{'){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()|| stack.peek()=='}'){
+                    stack.push(ch);
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        int ans=0,open=0,close=0;
+        while(!stack.isEmpty()){
+            char top=stack.pop();
+            if(top=='{'){
+                open++;
+            }else{
+                close++;
+            }
+        }
+        ans=(int)(Math.ceil(open/2.0)+Math.ceil(close/2.0));
+        return ans;
+    }
+}
