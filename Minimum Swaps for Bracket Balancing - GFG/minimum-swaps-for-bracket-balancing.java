4d9c9c4d37764b5b6c3bc342f87b1ea9a6fc4563
khushal0009 <98963826+khushal0009@users.noreply.github.com>
2023-05-26 14:05:04 +0530

    Added solution - LeetHub

---
+++ Minimum Swaps for Bracket Balancing - GFG/minimum-swaps-for-bracket-balancing.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {   
+            String S = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.minimumNumberOfSwaps(S));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int minimumNumberOfSwaps(String S){
+        // code here
+        int open=0,close=0,unbalanced=0,swaps=0;
+        for(int i=0;i<S.length();i++){
+            char ch=S.charAt(i);
+            if(ch=='['){
+                open++;
+                if(unbalanced>0){
+                    swaps+=unbalanced;
+                    unbalanced--;
+                }
+            }else{
+                close++;
+                unbalanced=close-open;
+            }
+        }
+        return swaps;
+    }
+}
