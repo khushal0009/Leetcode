b924563561bf937c37b0c99819e4bb050726db14
khushal0009 <98963826+khushal0009@users.noreply.github.com>
2023-06-03 21:42:30 +0530

    Time: 222 ms (28.46%), Space: 78 MB (36.15%) - LeetHub

---
+++ 1865-finding-pairs-with-a-certain-sum/1865-finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,30 @@
+class FindSumPairs {
+
+    private int [] nums1;
+    private int [] nums2;
+    Map<Integer, Integer> map = new HashMap<>();
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        for (int number : nums2) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        } 
+        
+    }
+    
+    public void add(int index, int val) {
+        map.put(nums2[index], map.get(nums2[index]) - 1);
+        nums2[index] += val;
+        map.put(nums2[index], map.getOrDefault(nums2[index], 0) + 1);
+    }
+    
+    public int count(int tot) {
+        int result = 0;
+        for (int number : nums1) {
+            if (map.containsKey(tot - number)) {
+                result += map.get(tot - number);
+            }
+        }
+        return result;
+    }
+}
